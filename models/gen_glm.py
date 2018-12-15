fedb707c7d86d281c8f9f550f0b17255d4503ed3
bmeatayi <bmeatayi@gmail.com>
2018-12-15 22:50:18 +0100

    added GLM generator with shared noise

---
+++ models/gen_glm.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+import numpy as np
+import torch
+
+
+class GeneratorGLM(nn.Module):
+    def __init__(self, latent_dim=1, stim_shape=(1, 1, 1), out_shape=(1, 1),
+                 sampler=torch.distributions.bernoulli.Bernoulli):
+        r"""
+        GLM based generator with shared noise
+        Args:
+            latent_dim (int): dimension of latent noise
+            stim_shape (tuple): shape of stimuli
+            out_shape (tuple): shape of output spikes
+            sampler (torch.distributions): sampler function (either Bernoulli or binary Gumbel-Softmax)
+        """
+        super(GeneratorGLM, self).__init__()
+
+        self.in_dim = np.prod(stim_shape)
+        self.latent_dim = np.prod(latent_dim)
+        self.n_t, self.n_cell = out_shape
+        self.glm_out_shape = np.prod(out_shape)
+        self.sampler = sampler
+
+        self.GLM = nn.Linear(self.in_dim, self.glm_out_shape)
+        self.shn_layer = nn.Linear(self.latent_dim, 1, bias=False)
+        print(self)
+
+    def forward(self, z, stim):
+        stim = stim.view(stim.size(0), -1)
+        x = self.GLM(stim) + self.shn_layer(z)
+        return x.view(x.size(0), self.n_t, self.n_cell)
+
+    def generate(self, z, stim):
+        r"""
+        Generate spikes
+        Args:
+            z (tensor): input noise
+            stim (tensor): stimulus
+
+        Returns:
+            spikes (tensor): discrete spikes
+        """
+        with torch.no_grad():
+            logits = self.forward(z, stim)
+            sampling_distribution = self.sampler(logits=logits)
+        spikes = sampling_distribution.sample()
+        return spikes
