8ba672c4b3c4bb68c3f319fcfc414e6906f91aa3
bmeatayi <bmeatayi@gmail.com>
2018-12-19 00:51:30 +0100

    another run!

--- examples/run1.sh
+++ examples/run1.sh
@@ -11,13 +11,13 @@
 #SBATCH --get-user-env
 
 #SBATCH --mail-type=end
-#SBATCH --mem=6000mb
+#SBATCH --mem=8000mb
 
 #SBATCH --mail-user=m.atayi@tum.de
 
 #SBATCH --export=NONE
 
-#SBATCH --time=08:00:00
+#SBATCH --time=02:30:00
 
 source /etc/profile.d/modules.sh
 
@@ -27,8 +27,10 @@ source activate rvdev
 
 cd /home/hpc/pn56ju/ga48zop2/neurogan/examples/
 
-#python glm30n.py
+python glm30n.py
 
-python run_cgan.py --spike_file ..//dataset//GLM_2D_30n_shared_noise//data.npy --stim_file ..//dataset//GLM_2D_30n_shared_noise//stim.npy --log_dir cgan_results//SharedNoise_30N_run01_lam.1_gs//
+# wgan-gp with gs (default)
+# python run_cgan.py --n_epochs 1500 --gs_temp 1 --log_dir cgan_results//SharedNoise_30N_run02_lam.1_gs_temp1// --spike_file ..//dataset//GLM_2D_30n_shared_noise//data.npy --stim_file ..//dataset//GLM_2D_30n_shared_noise//stim.npy
 
-
+# wgan-gp with REINFORCE
+# python run_cgan.py --lr 0.0001 --n_epochs 2000 --grad_mode reinforce --log_dir cgan_results//reinforce__30N_run01// --spike_file ..//dataset//GLM_2D_30n_shared_noise//data.npy --stim_file ..//dataset//GLM_2D_30n_shared_noise//stim.npy
