a68e88390e48c45e89da466c914b34daf6eeff8a
bmeatayi <bmeatayi@gmail.com>
2019-01-08 11:46:41 +0100

    updated for new discriminator

--- examples/cnn10n.py
+++ examples/cnn10n.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
@@ -15,7 +16,7 @@ from utils.cgan_dataset import GanDataset
 spike_file = '..//dataset//cnn_data//spike.npy'
 stim_file = '..//dataset//cnn_data//stim.npy'
 
-log_folder = 'cnn_results//run01_gs_anneal_//'
+log_folder = 'cnn_results//run08_gs.5_ndisc5_lr1e-5_dChanged//'
 
 batch_size = 128
 N = 10
@@ -40,9 +41,14 @@ generator = GeneratorCNN(nw=nx, nh=nx, nl=nt,
                          n_cell=N)
 
 discriminator = DiscriminatorCNN(nw=nx, nh=nx, nl=nt,
-                         n_filters=(3, 2),
-                         kernel_size=(f1sz, f2sz),
-                         n_cell=N)
+                                 n_filters=(3, 2),
+                                 kernel_size=(f1sz, f2sz),
+                                 n_cell=N,
+                                 spectral_norm=True,
+                                 mid_act_func=nn.LeakyReLU(0.2, inplace=True),
+                                 n_units=[128, 256, 512, 256, 128],
+                                 p_drop=None
+                                 )
 
 solver = TrainerCGAN(optimizer_g=torch.optim.Adam,
                      optimizer_d=torch.optim.Adam,
@@ -50,15 +56,13 @@ solver = TrainerCGAN(optimizer_g=torch.optim.Adam,
                      gan_mode='sn',
                      lambda_gp=None,
                      grad_mode='gs',
-                     gs_temp=1,
+                     gs_temp=.5,
                      n_neuron=N)
 
 solver.train(generator=generator, discriminator=discriminator,
              train_loader=train_dataloader, val_loader=val_dataloader,
-             lr=1e-4, b1=.5, b2=0.999,
-             log_interval=10000, n_epochs=3500,
-             n_disc_train=5,
-             temp_anneal=.9995
+             lr=1e-5, b1=.5, b2=0.999,
+             log_interval=5000, n_epochs=1000,
+             n_disc_train=1,
+             temp_anneal=1.0
              )
-
-
